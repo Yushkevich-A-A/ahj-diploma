d37fccc292b7381d06ff8421ebd90a1e7ba3daa1
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-08-29 00:21:43 +0300

    connect to web socket

--- src/js/Widget/DrawWidget/DrawWidget.js
+++ src/js/Widget/DrawWidget/DrawWidget.js
@@ -1,13 +1,18 @@
 // images
-
 import logo from './images/logo.png';
 import emoji from './images/buttons/emoji.png';
 import paperclip from './images/buttons/paperclip.png';
 import send from './images/buttons/send.png';
 
+import moment from 'moment';
+import 'moment/locale/ru';
+
+moment().locale('ru');
+
 export default class DrawWidget {
     constructor(element = null) {
         this.element = element;
+        this.inputData = null;
         this.init();
     }
 
@@ -52,52 +57,67 @@ export default class DrawWidget {
             </div>
             <div class="block-form-input-content">
                 <form class="form-input-content">
-                <div class="basil-function">
-                    <div class="button button-paperclip">
-                    <img    class="button-paperclip__img" src="${emoji}" alt="">
-                    </div>
-                    <div class="button button-emoji">
-                      <img class="button-emoji__img" src="${paperclip}" alt="">
-                    </div>
-                    <input class="input form-input-content__input" type="text" value="Type your message here">
-                        <div class="button button-send-message">
-                            <img class="button-send-message__img" src="${send}" alt="">
+                    <div class="basil-function">
+                        <div class="add-emoji-files">
+                            <div class="button add-emoji-files-button button-paperclip ">
+                                <img class="button-paperclip__img" src="${paperclip}" alt="">
+                            </div>
+                            <div class="button add-emoji-files-button button-emoji">
+                                <img class="button-emoji__img"  src="${emoji}" alt="">
+                            </div>
                         </div>
-                </div>
-                <div class="additional-function">
-                </div>
+                        <div class="send-message">
+                            <div class="send-message-input">
+                                <textarea class="input form-input-content__input" type="text" placeholder="Type your message here"></textarea>
+                            </div>
+                            <div class="button button-send-message">
+                                <img class="button-send-message__img" src="${send}" alt="">
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
             </div>
         </div>`;
         this.element.appendChild(this.widget);
         this.contentList = this.widget.querySelector('.list-content');
+        this.blockDisplayContent = this.widget.querySelector('.block-display-content');
         this.additionalMenu = this.widget.querySelector('.additional-menu');
-
+        this.formInput = this.widget.querySelector('.form-input-content');
+        this.formInputContent = this.widget.querySelector('.form-input-content__input');
     }
 
     drawContentWidget(data) {
         for(let i of data) {
-            this.drawTextContent(i);
+            this.drawContent(i);
         }
     }
 
-    drawContent(data) {
+    drawContent(item, newMessage = false) {
+        console.log(item)
         const li = document.createElement('li');
         li.classList.add('item-content');
-        li.innerHTML = `<div class="message-date"></div>`;
-        this.contentList.insertAdjacentElement("beforeend", li);
+        li.innerHTML = `<div class="content-message">
+                            <div class="message-date"></div>
+                        </div>`;
+        li.dataset.idPost = item.data.id;
+        if (newMessage) {
+            this.contentList.appendChild(li);
+        } else {
+            this.contentList.insertAdjacentElement("beforeend", li); 
+        }
         const messageDate = li.querySelector('.message-date');
-        messageDate.textContent = moment(data.content.date).format('DD.MM.YYYY HH:mm');
+        messageDate.textContent = moment(item.data.date).format('DD.MM.YYYY HH:mm');
+        const contentMessage = li.querySelector('.content-message');
 
         let content;
-        switch (data.type) {
+        switch (item.type) {
             case 'text':
-                content = this.newText(data.content.text);
+                content = this.newText(item.data.content);
                 break;
         }
 
-        this.contentList.appendChild(content);
+        contentMessage.appendChild(content);
     }
 
     newText(data) {
@@ -113,5 +133,21 @@ export default class DrawWidget {
             return;
         }
         this.additionalMenu.classList.add('disable');
+    }
+
+    validityInput() {
+        this.inputData = null;
+        if (this.formInputContent.value.trim() === '') {
+            this.formInput.reset();
+            return false;
+        }
+
+        this.inputData = this.formInputContent.value.trim();
+        this.formInput.reset();
+        return this.inputData;
+    }
+
+    scrollToBottom() {
+        this.blockDisplayContent.scrollTop = this.blockDisplayContent.scrollHeight
     }
 }
--- src/js/Widget/WidgetController/WidgetController.js
+++ src/js/Widget/WidgetController/WidgetController.js
@@ -1,11 +1,13 @@
 export default class WidgetController {
     constructor(widget) {
         this.widget = widget;
+        this.typeInput = 'text';
         this.init();
     }
 
     init() {
         this.listeners();
+        this.connectionToServer();
     }
 
     listeners() {
@@ -14,6 +16,49 @@ export default class WidgetController {
             if (event.target.closest('.logo-menu')) {
                 this.widget.openAddMenu();
             }
+
+            if (event.target.closest('.button-send-message')) {
+                this.sentDataToServer();
+            }
         })
+    }
+
+    connectionToServer() {
+        this.ws = new WebSocket('ws://localhost:7070/');
+
+        this.ws.addEventListener('open', () => {
+            console.log('соединение открыто');
+        });
+
+        this.ws.addEventListener('message', (event) => {
+            const data = JSON.parse(event.data);
+            console.log(data);
+            if(data.status === 'init') {
+                this.widget.drawContentWidget(data.data);
+                this.widget.scrollToBottom()
+            }
+
+            if (data.status === 'message') {
+                this.widget.drawContent(data.data, true);
+            }
+        });
+    }
+
+    sentDataToServer() {
+        if (this.typeInput === 'text') {
+            const data = this.widget.validityInput();
+            if (!data) {
+                return;
+            }
+            const obj = {
+                type: 'text',
+                data: {
+                    content: data,
+                    date: Date.now(),
+                }
+            };
+            const string = JSON.stringify(obj);
+            this.ws.send(string);
+        }
     }
 }
